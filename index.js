a667978f575fc95d5154a7496693e8f81d74e674
Sebastian Zambrano <sebastian9zambrano10@gmail.com>
2024-06-10 20:57:01 -0500

    Tarea

---
+++ index.js
@@ -0,0 +1,20 @@
+'Use Strict';
+//1
+let letras = ["A", "B", "C", "D", "E"];
+for (let i = 0; i < letras.length; i++) {
+    alert("Ubicacion " + i + " Elemento " + letras[i]);
+}
+
+//2
+let suma = 0;
+let number = [12, 24, 36, 48, 60, 72];
+for (let i = 0; i < 6; i++){
+    suma = suma + number[i];
+        alert ("El resultado de la suma es " + suma);
+}
+
+//3
+let numeros = [1, 3, 5, 7, 9, 12]
+for (let i = 0; i < numeros.length; i++)[
+    alert (numeros[i] * 3)
+]
